0be750005462bffffbfc2a061eb0263e980adb87
Denis <nebyt@mail.ru>
2017-06-25 23:46:12 +0300

    view log in real time

---
+++ logViewerProc.py
@@ -0,0 +1,20 @@
+import time
+
+def tail(f):
+    f.seek(0,2) # переходим в конец файла
+    while True:
+        line = f.readline() # читаем следующую строку
+        if not line:  # если строки нет - ждем
+            time.sleep(0.1)
+            continue
+        yield line
+
+def openLog():
+    pathToFile = input('Enter the path to the file: ').strip('"')
+    f = open(pathToFile,'r')
+    print(f.read(),end = '')
+    logStr = tail(f)
+    for line in logStr:
+        print(line.strip())
+
+openLog()
