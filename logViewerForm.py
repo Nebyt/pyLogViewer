808d99265ec83245e9fe8c78ddc9953e0bacac54
Denis <nebyt@mail.ru>
2017-07-06 07:51:30 +0300

    in progress...

--- logViewerForm.py
+++ logViewerForm.py
@@ -2,32 +2,44 @@ import tkinter
 from tkinter.filedialog import askopenfilename
 import logViewerProc
 import threading
+import os
+import time
 
 
 # функция стартер
 def starter():
     # поток для просмотра файла
-    th = threading.Thread(target=logViewerProc.open_file, args=(r'{}'.format(path_to_file()),), name= 1)
-    th.start()
+    th1 = threading.Thread(target = logViewerProc.open_file, args=(r'{}'.format(path_to_file()),),daemon = True , name= 'read file')
+    th2 = threading.Thread(target = listen_pipe, daemon = True, name= 'listen_pipe')
+    th1.start()
+    th2.start()
 
 def path_to_file():
     op = askopenfilename()
     return op
 
+def listen_pipe():
+    while True:
+        read_from_pipe = os.fdopen(logViewerProc.r_file,'r', 1)
+        str = read_from_pipe.read()
+        if not str:
+            continue
+        else:
+            txt.insert(tkinter.END,str)
+
+
 # создаем GUI
 root = tkinter.Tk()
 screen_width = root.winfo_screenwidth()
-print(screen_width)
 screen_height = root.winfo_screenheight()
-print(screen_height)
 root.wm_title("LogViewer")
 
 
 m = tkinter.Menu(root)
 root.config(menu=m)
-m.add_command(label="Open...", command=starter)
 txt = tkinter.Text(root, width=90, height=30, font="12")
 txt.pack()
+m.add_command(label="Open...", command=starter)
 
 if __name__ == '__main__':
     root.mainloop()
--- logViewerProc.py
+++ logViewerProc.py
@@ -1,5 +1,7 @@
-import time,threading
+import time
+import os
 
+r_file,w_file = os.pipe()
 #  постоянное сканирование файла
 def tail(f):
     f.seek(0,2) # переходим в конец файла
@@ -13,9 +15,12 @@ def tail(f):
 # открываем нужный файл для просмотра
 def open_file(path_to_file):
     f = open(path_to_file,'r')
-    print(f.read())
-    print(f.tell())
-    #logViewerForm.txt.insert(END,f.read()) # заменить на то что не будет вызывать импорт
+    write_to_pipe = os.fdopen(w_file,'w')
+    file_before = f.read()
+    for file_line in file_before:
+        write_to_pipe.write(f.read())
+    #logViewerForm.txt.insert(tkinter.END,f.read()) # заменить на то что не будет вызывать импорт
     for line in tail(f):
-        print(f.tell())
-        #logViewerForm.txt.insert(END,line)# заменить на то что не будет вызывать импорт
+        write_to_pipe = os.fdopen(w_file, 'w')
+        write_to_pipe.write(line)
+            # logViewerForm.txt.insert(tkinter.END,line)# заменить на то что не будет вызывать импорт
